dd6d7ecd7791a750ea5dd277c4fcfe3f33085347
Eric Tytell <eric.tytell@tufts.edu>
2021-07-22 14:08:38 -0400

    Calculates stiffnesses and damping. Works with frequency sweeps

--- bender_data.R
+++ bender_data.R
@@ -116,6 +116,26 @@ load_bender_data <- function(filename, cycle_offset=0, filterorder=7, filtercuto
                    height.m = h5attr(h5file, 'FishCrossSectionHeight_mm')/1000,
                    sample_frequency.Hz = h5attr(h5file[['/RawInput']], 'SampleFrequency')))
         
+        filetype <- h5attr(h5file[['/NominalStimulus']], 'Type')
+        if (filetype == "Frequency Sweep") {
+          # t.norm is in radians in frequency sweeps, just to be confusing
+          dur <- h5attr(h5file[['/NominalStimulus']], 'Duration')
+          data <- mutate(data,
+                         t.norm = t.norm / (2*pi),
+                         frequency.Hz = (lead(t.norm) - lag(t.norm)) / 
+                           (lead(t.s) - lag(t.s)))
+          ncyc <- data %>%
+            filter(t.s < dur) %>%
+            summarize(ncyc = last(t.norm)) %>%
+            pull(ncyc)
+          
+          data <- mutate(data,
+                         t.norm = case_when(t.norm < 0.25  ~  NA_real_,
+                                            t.norm > ncyc - 0.25  ~  NA_real_,
+                                            TRUE  ~  t.norm),
+                         frequency.Hz = if_else(is.na(t.norm), NA_real_, frequency.Hz))
+        }
+
         if ("Amplitude" %in% h5attr_names(h5file[['/NominalStimulus']])) {
           data$amplitude.deg = h5attr(h5file[['/NominalStimulus']], 'Amplitude')
         }
@@ -125,16 +145,24 @@ load_bender_data <- function(filename, cycle_offset=0, filterorder=7, filtercuto
         
         sampfreq <- first(pull(data, sample_frequency.Hz))
         
-        bf <- signal::butter(filterorder, filtercutoff / sampfreq, type = 'low')
+        bf <- signal::butter(filterorder, filtercutoff / (0.5*sampfreq), type = 'low')
         data <- mutate(data,
                        torque.Nm.orig = torque.Nm,
-                       torque.Nm = signal::filtfilt(bf, torque.Nm))
+                       torque.Nm = signal::filtfilt(bf, torque.Nm.orig),
+                       curve.invm.orig = curve.invm,
+                       curve.invm = signal::filtfilt(bf, curve.invm.orig))
       }
     },
     finally = {
       h5file$close_all()
     }
   )
+
+  data <-
+    data %>%
+    mutate(torquebias.Nm = mean(torque.Nm[t.s < -0.5], na.rm = TRUE),
+           torque.Nm.ctr = torque.Nm - torquebias.Nm,
+           curverate.invms = (lead(curve.invm) - lag(curve.invm)) / (lead(t.s) - lag(t.s)))
 
   return(data)
 }
@@ -144,47 +172,127 @@ load_cycle_data <- function(filename, tsdata) {
     {
       h5file <- H5File$new(filename, mode='r')
 
-      cycledata1 <- tibble(amplitude.deg = h5attr(h5file[['/NominalStimulus']], 'AmplitudeByCycle'),
-                          frequency.Hz = h5attr(h5file[['/NominalStimulus']], 'FrequencyByCycle'),
-                          is_active = h5attr(h5file[['/NominalStimulus']], 'IsActiveByCycle'),
-                          activation_duty = h5attr(h5file[['/NominalStimulus']], 'ActualActivationDuty')) %>%
-        mutate(is_active = factor(if_else(is_active, 'active', 'passive'), levels=c('passive', 'active')))
-      
-      cycledata1$activation_phase <- h5attr(h5file[['/NominalStimulus']], 'ActivationPhase')
-      cycledata1 <-
-        mutate(cycledata1, 
-               halfcycle = seq(0, length.out = nrow(cycledata1)),
-               side = factor('left', levels=c('left', 'right')))
-      
-      cycledata2 <- mutate(cycledata1,
-                           halfcycle = halfcycle+0.5,
-                           side = factor('right', levels=c('left', 'right')))
+      filetype <- h5attr(h5file[['/NominalStimulus']], 'Type')
+      if (filetype == 'Frequency Sweep') {
+         cycledata <-
+           tsdata %>%
+           filter(t.s >= 0 & !is.na(halfcycle)) %>%
+           group_by(halfcycle) %>%
+           summarize(
+             t_cycle.s = first(t.s),
+             amplitude.deg = max(abs(angle.deg)),
+             is_active = factor('passive', levels=c('passive', 'active')),
+             activation_duty = 0,
+             activation_phase = NA_real_,
+             t_act_on.s = NA_real_,
+             t_act_off.s = NA_real_,
+             t_act_on.norm = NA_real_,
+             t_act_off.norm = NA_real_,
+             curvature.invm = amplitude.deg * pi/180 / first(tsdata$dclamp.m))
+         
+         # we use 0.5 / (stuff) here, because we're grouping things by half cycles
+         cycledata <-
+           cycledata %>%
+           mutate(frequency.Hz = 0.5 / (lead(t_cycle.s) - t_cycle.s),
+                  side = factor(case_when(halfcycle %% 1 < 0.1  ~  'left',
+                                          halfcycle %% 1 >= 0.4  ~  'right'),
+                                levels = c('left', 'right')))
+         # the first value is usually messed up because of the ramp to position
+         cycledata$frequency.Hz[1] = NA_real_
+      }
+      else if ('AmplitudeByCycle' %in% h5attr_names(h5file[['/NominalStimulus']])) {
+        cycledata1 <- tibble(amplitude.deg = h5attr(h5file[['/NominalStimulus']], 'AmplitudeByCycle'),
+                            frequency.Hz = h5attr(h5file[['/NominalStimulus']], 'FrequencyByCycle'),
+                            is_active = h5attr(h5file[['/NominalStimulus']], 'IsActiveByCycle'),
+                            activation_duty = h5attr(h5file[['/NominalStimulus']], 'ActualActivationDuty')) %>%
+          mutate(is_active = factor(if_else(is_active, 'active', 'passive'), levels=c('passive', 'active')))
+        
+        cycledata1$activation_phase <- h5attr(h5file[['/NominalStimulus']], 'ActivationPhase')
+        cycledata1 <-
+          mutate(cycledata1, 
+                 halfcycle = seq(0, length.out = nrow(cycledata1)),
+                 side = factor('left', levels=c('left', 'right')))
+        
+        cycledata2 <- mutate(cycledata1,
+                             halfcycle = halfcycle+0.5,
+                             side = factor('right', levels=c('left', 'right')))
+    
+        cycledata <- bind_rows(cycledata1, cycledata2)
+        
+        tcycle <-
+          tsdata %>%
+          filter(t.s >= 0) %>%
+          group_by(halfcycle) %>%
+          summarize(t_cycle.s = min(t.s))
+        
+        cycledata <- left_join(cycledata, tcycle, by="halfcycle")
   
-      cycledata <- bind_rows(cycledata1, cycledata2)
-      
-      tcycle <-
-        tsdata %>%
-        filter(t.s >= 0) %>%
-        group_by(halfcycle) %>%
-        summarize(t_cycle.s = min(t.s))
-      
-      cycledata <- left_join(cycledata, tcycle, by="halfcycle")
+        cycledata <-
+          cycledata %>%
+          mutate(t_act_on.s = t_cycle.s + (0.25 + activation_phase)/frequency.Hz,
+                 t_act_off.s = t_act_on.s + activation_duty/frequency.Hz,
+                 t_act_on.norm = halfcycle + 0.25 + activation_phase,
+                 t_act_off.norm = t_act_on.norm + activation_duty,
+                 activation_phase = case_when(side == 'right'  ~  activation_phase+0.5,
+                                              side == 'left'   ~  activation_phase),
+                 curvature.invm = amplitude.deg * pi/180 / first(tsdata$dclamp.m))
+        
+        # Set the activation times to NA if there wasn't any activation in this cycle
+        cycledata <-
+          cycledata %>%
+          mutate(across(c(t_act_on.s, t_act_off.s, activation_phase), 
+                        ~ if_else(is_active == 'active', .x, NA_real_))) %>%
+          arrange(halfcycle)
+      }
+      else {
+        ncycles <- h5attr(h5file[['/NominalStimulus']], 'Cycles')
+        
+        cycledata1 <- tibble(
+          cycle = seq(0, length.out = ncycles),
+          halfcycle = seq(0, length.out = ncycles),
+          amplitude.deg = h5attr(h5file[['/NominalStimulus']], 'Amplitude'),
+          frequency.Hz = h5attr(h5file[['/NominalStimulus']], 'Frequency'),
+          activation_duty = h5attr(h5file[['/NominalStimulus']], 'ActivationDuty'))
 
-      cycledata <-
-        cycledata %>%
-        mutate(t_act_on.s = t_cycle.s + (0.25 + activation_phase)/frequency.Hz,
-               t_act_off.s = t_act_on.s + activation_duty/frequency.Hz,
-               t_act_on.norm = halfcycle + 0.25 + activation_phase,
-               t_act_off.norm = t_act_on.norm + activation_duty,
-               activation_phase = case_when(side == 'right'  ~  activation_phase+0.5,
-                                            side == 'left'   ~  activation_phase))
-      
-      # Set the activation times to NA if there wasn't any activation in this cycle
-      cycledata <-
-        cycledata %>%
-        mutate(across(c(t_act_on.s, t_act_off.s, activation_phase), 
-                      ~ if_else(is_active == 'active', .x, NA_real_))) %>%
-        arrange(halfcycle)
+        actstartcycle = h5attr(h5file[['/NominalStimulus']], 'ActivationStartCycle')
+        cycledata1 <- cycledata1 %>%
+          mutate(is_active = case_when(cycle < actstartcycle  ~  'passive',
+                                       TRUE  ~  'active'),
+                 is_active = factor(is_active, levels=c('passive', 'active')),
+                 activation_phase = h5attr(h5file[['/NominalStimulus']], 'ActivationStartPhase'),
+                 side = factor('left', levels=c('left', 'right')))
+        
+        cycledata2 <- mutate(cycledata1,
+                             halfcycle = halfcycle+0.5,
+                             side = factor('right', levels=c('left', 'right')))
+        
+        cycledata <- bind_rows(cycledata1, cycledata2)
+        
+        tcycle <-
+          tsdata %>%
+          filter(t.s >= 0) %>%
+          group_by(halfcycle) %>%
+          summarize(t_cycle.s = min(t.s))
+        
+        cycledata <- left_join(cycledata, tcycle, by="halfcycle")
+        
+        cycledata <-
+          cycledata %>%
+          mutate(t_act_on.s = t_cycle.s + (0.25 + activation_phase)/frequency.Hz,
+                 t_act_off.s = t_act_on.s + activation_duty/frequency.Hz,
+                 t_act_on.norm = halfcycle + 0.25 + activation_phase,
+                 t_act_off.norm = t_act_on.norm + activation_duty,
+                 activation_phase = case_when(side == 'right'  ~  activation_phase+0.5,
+                                              side == 'left'   ~  activation_phase),
+                 curvature.invm = amplitude.deg * pi/180 / first(tsdata$dclamp.m))
+        
+        # Set the activation times to NA if there wasn't any activation in this cycle
+        cycledata <-
+          cycledata %>%
+          mutate(across(c(t_act_on.s, t_act_off.s, activation_phase), 
+                        ~ if_else(is_active == 'active', .x, NA_real_))) %>%
+          arrange(halfcycle)
+      }
     },
     finally = {
       h5file$close_all()
@@ -196,6 +304,20 @@ load_cycle_data <- function(filename, tsdata) {
 
 get_avg_stiffness <- function(curve, torque) {
   s <- svd(cbind(curve, torque))
+  if (all(s$v[,1] < 0)) {
+    return(-s$v[2,1])
+  }
+  else {
+    return(s$v[2,1])
+  }
+}
+
+get_avg_damping <- function(curverate, torque) {
+  if (any(is.na(curverate)) | any(is.na(torque))) {
+    return(NA_real_)
+  }
+    
+  s <- svd(cbind(curverate, torque))
   if (all(s$v[,1] < 0)) {
     return(-s$v[2,1])
   }
@@ -234,36 +356,59 @@ get_low_curve_stiffness <- function(curve, torque, sign=1,
 }
 
 calculate_cycle_data <- function(rawdata,
-                           low_angvel = 10,
+                           low_curvevel = 1,
                            low_curve = 0.2) {
+  rawdata <- rawdata %>%
+    mutate(midcycle = floor((t.norm-0.25)*2)/2 + 0.25)
+  
   cycledata <- rawdata %>%
-    group_by(cycle) %>%
+    group_by(halfcycle) %>%
     summarize(
       work = pracma::trapz(-angle.deg * pi/180, torque.Nm),
       EI1 = get_avg_stiffness(curve.invm, torque.Nm.ctr),
       # stiffness when curvature is large = max torque / max curve
       # add some robustness by using the 99th percentile rather than max
-      EIl1 = median(torque.Nm.ctr[curve.invm >= quantile(curve.invm, 0.99)], na.rm = TRUE) / quantile(curve.invm, 0.99),
-      EIl2 = median(torque.Nm.ctr[curve.invm <= quantile(curve.invm, 0.01)], na.rm = TRUE) / quantile(curve.invm, 0.01))
-      
-  rawdata <- mutate(rawdata,
-                    dangle.degs = (lead(angle.deg) - lag(angle.deg)) / (lead(t.s) - lag(t.s)))
+      EIL = median(torque.Nm.ctr[abs(curve.invm) >= quantile(abs(curve.invm), 0.98)], na.rm = TRUE) / quantile(abs(curve.invm), 0.98),
+      EIL = abs(EIL),
+      etaI1 = get_avg_damping(curverate.invms, torque.Nm.ctr),
+      #damping when the curvature rate is maximal
+      curveratemax.invms = quantile(abs(curverate.invms), 0.98, na.rm=TRUE),
+      etaIL = median(torque.Nm.ctr[abs(curverate.invms) >= curveratemax.invms], na.rm = TRUE) / 
+        curveratemax.invms,
+      etaIL = abs(etaIL))
+      # EIl2 = median(torque.Nm.ctr[curve.invm <= quantile(curve.invm, 0.01)], na.rm = TRUE) / quantile(curve.invm, 0.01))
   
-  EIm1 <- rawdata %>%
-    filter((dangle.degs > low_angvel) & (abs(curve.invm) < low_curve)) %>%    # gets high positive angular velocity and low curvature
-    group_by(cycle) %>%
-    summarize(EIm1 = diff(range(torque.Nm)) / diff(range(curve.invm))) # this is the slope of torque vs curvature when curvature is small
+  # here we have to group differently, because we're looking for the middle of each cycle (from phase 0.25 to 0.75 and 0.75 back to 0.25),
+  # which is when the velocities are high and the curvature is near zero
+  EIM <- rawdata %>%
+    filter((abs(curverate.invms) > low_curvevel) & (abs(curve.invm) < low_curve)) %>%    # gets high curvature velocity and low curvature
+    group_by(midcycle) %>%
+    summarize(
+      # this is the slope of torque vs curvature when curvature is small 
+      ## TODO: maybe think if this is the best algorithm
+      EIM = diff(range(torque.Nm.ctr)) / diff(range(curve.invm)), 
+      # this spans halfcycles, but we'll attribute the stiffness to the first half cycle
+      halfcycle = first(midcycle)-0.25)
   
-  EIm2 <- rawdata %>%
-    filter((dangle.degs < -low_angvel) & (abs(curve.invm) < low_curve)) %>%    # gets high negative angular velocity and low curvature
-    group_by(cycle) %>%
-    summarize(EIm2 = diff(range(torque.Nm)) / diff(range(curve.invm))) # this is the slope of torque vs curvature when curvature is small
+  # here we want small curvature rates, but large curvature
+  etaIM <- rawdata %>%
+    filter((abs(curverate.invms) < low_curvevel) & (abs(curve.invm) > low_curve)) %>%    # gets low curvature velocity and high curvature
+    group_by(midcycle) %>%
+    summarize(
+      # this is the slope of torque vs curvature when curvature is small
+      etaIM = diff(range(torque.Nm.ctr)) / diff(range(curverate.invms)), 
+      # this spans halfcycles, but we'll attribute the stiffness to the first half cycle
+      halfcycle = first(midcycle)-0.25)
+
+  # EIm2 <- rawdata %>%
+  #   filter((dangle.degs < -low_angvel) & (abs(curve.invm) < low_curve)) %>%    # gets high negative angular velocity and low curvature
+  #   group_by(cycle) %>%
+  #   summarize(EIm2 = diff(range(torque.Nm)) / diff(range(curve.invm))) # this is the slope of torque vs curvature when curvature is small
   
   cycledata <- cycledata %>%
-    left_join(EIm1, by="cycle") %>%
-    left_join(EIm2, by="cycle")
+    left_join(EIM, by="halfcycle") %>%
+    left_join(etaIM, by="halfcycle")
 
-  stim <- tibble(cycle=seq())
   return(cycledata)
 }
 
