8a1447fb481029ed4a490dddaf934819725c51ab
yordanojimenez <jimenez.yordano@gmail.com>
2026-03-06 17:31:38 -0500

    Modified function names (activation to stim)

--- bender_functions.py
+++ bender_functions.py
@@ -26,15 +26,15 @@ import h5py
 
 class Bender:
     def __init__(self):
-        self.S1actcmd = None
-        self.S2actcmd = None
+        self.S1stimcmd = None
+        self.S2stimcmd = None
 #Sets up the specific parameters for a given test type using data stored in self and general parameters passed via kwargs.
 
     def run_experiment(self, device_name = "Dev1", test_type = "dynamic"):
 
-        # Replace ... with actual channel names as needed (Operate on self)
+        # Replace ... with stimual channel names as needed (Operate on self)
         self.set_input_channels(inchannels=['ai0'], inchannel_names=['Fx'])
-        self.set_activation_channels('ao0', 'ao1')
+        self.set_stimulation_channels('ao0', 'ao1')
         # self.calibration is assumed to be loaded in the notebook using bender.loadCalibration(...)
 
         # General parameters (duration/sample_rate passed from notebook)
@@ -74,7 +74,7 @@ class Bender:
         else:
             raise ValueError(f"Unknown test type: {test_type}")
     
-        # NOW ACTUALLY MAKE THE SIGNAL FOR THE MOTOR (BASED ON PREVIOUS DYNAMIC, SWEEP, ETC)
+        # NOW stimUALLY MAKE THE SIGNAL FOR THE MOTOR (BASED ON PREVIOUS DYNAMIC, SWEEP, ETC)
         # Set the generated signals for     the Bender instance ('self') to use in .run()
         self.set_bending_signal(self.t, angle, anglevel, tnorm) # Pass tnorm here as well
 
@@ -82,24 +82,24 @@ class Bender:
         # NOW LET'S ADD ELECTRICAL STIMULI!
         # Access stim parameters stored in 'self' (set by organize_cycles)
         stimulation_params = {
-            'is_activation': self.is_activation, # Must be set in notebook
-            'activation_pulse_rate': self.activation_pulse_rate, # Must be set in notebook
+            'is_stimulation': self.is_stimulation, # Must be set in notebook
+            'stimulation_pulse_rate': self.stimulation_pulse_rate, # Must be set in notebook
             'prestim_time': self.prestim_time, # Must be set in notebook
             'poststim_time': self.poststim_time, # Must be set in notebook
             'prepoststim_dur': self.prepoststim_dur, # Must be set in notebook
             'prepoststim_sep': self.prepoststim_sep, # Must be set in notebook
             'movedur': duration, 
-            'actburstdur': self.actburstdur,        
+            'stimburstdur': self.stimburstdur,        
             'duty_by_cycle': self.duty_by_cycle,     
             'freq_by_cycle': self.freq_by_cycle,    
             'phase_by_cycle': self.phase_by_cycle, 
             }
 
         # FINISH Generate electrical stimuli
-        S1actcmd, S2actcmd = self._make_stimuli(**stimulation_params)
+        S1stimcmd, S2stimcmd = self._make_stimuli(**stimulation_params)
             
-        # Set the generated activation signals in 'self'
-        self.set_activation(S1actcmd, S2actcmd)
+        # Set the generated stimulation signals in 'self'
+        self.set_stimulation(S1stimcmd, S2stimcmd)
         self.make_motor_stepper_pulses(outputfreq=self.outputfreq)
         filename = self.increment_file_name(f'experiment_data_{test_type}_000.h5')
         print(f"Data will be saved to: {filename}")
@@ -109,7 +109,7 @@ class Bender:
 
 
 
-    def organize_cycles(self, curves, frequencies, randomize, cycles_per_step, n_end_cycles, dclamp, xsec_width, stim_cycles_in_step, activation_duties, activation_phases, activation_pulse_rate):
+    def organize_cycles(self, curves, frequencies, randomize, cycles_per_step, n_end_cycles, dclamp, xsec_width, stim_cycles_in_step, stimulation_duties, stimulation_phases, stimulation_pulse_rate):
     # Calculates the full sequence of frequencies, amplitudes, and periods by cycle, based on initial lists of curves/frequencies and test parameters.
 
 
@@ -121,8 +121,8 @@ class Bender:
 
         for c1 in curves:
             for f1 in frequencies:
-                for d1 in activation_duties:  # <--- Loop through duties
-                    for p1 in activation_phases: # <--- Loop through phases
+                for d1 in stimulation_duties:  # <--- Loop through duties
+                    for p1 in stimulation_phases: # <--- Loop through phases
                         allfreqs.append(f1)
                         allcurves.append(c1)
                         all_duties.append(d1) # <--- Append duty
@@ -173,15 +173,15 @@ class Bender:
             raise IndexError("stim_cycles_in_step have to be less than cycles_in_step")
 
         c = np.arange(0, cycles_per_step)
-        is_act_cycle = np.isin(c, stim_cycles_in_step)
-        is_act_cycle = np.tile(is_act_cycle, len(allfreqs))
-        is_act_cycle = np.concatenate((is_act_cycle, [False] * n_end_cycles))
+        is_stim_cycle = np.isin(c, stim_cycles_in_step)
+        is_stim_cycle = np.tile(is_stim_cycle, len(allfreqs))
+        is_stim_cycle = np.concatenate((is_stim_cycle, [False] * n_end_cycles))
 
 
-        # Calculate activation makeburst duration
-        actburstdur = duty_by_cycle / freq_by_cycle 
-        actburstdur = np.floor(actburstdur * activation_pulse_rate * 2) / (activation_pulse_rate * 2)
-        actburstdur[is_act_cycle == False] = 0
+        # Calculate stimulation makeburst duration
+        stimburstdur = duty_by_cycle / freq_by_cycle 
+        stimburstdur = np.floor(stimburstdur * stimulation_pulse_rate * 2) / (stimulation_pulse_rate * 2)
+        stimburstdur[is_stim_cycle == False] = 0
 
         # --- Store all results as INSTANCE ATTRIBUTES (self.) ---
         self.period_by_cycle = period_by_cycle
@@ -192,7 +192,7 @@ class Bender:
         self.allfreqs = allfreqs
         self.allcurves = allcurves
         self.allamps = allamps
-        self.actburstdur = actburstdur
+        self.stimburstdur = stimburstdur
         self.duty_by_cycle = duty_by_cycle # Store the final duty array
         self.allstrains = allstrains
         self.allstrainrates = allstrainrates
@@ -211,9 +211,9 @@ class Bender:
         else:
             self.tnorm = tnorm
     
-    def set_activation(self, S1actcmd, S2actcmd):
-        self.S1actcmd = S1actcmd
-        self.S2actcmd = S2actcmd
+    def set_stimulation(self, S1stimcmd, S2stimcmd):
+        self.S1stimcmd = S1stimcmd
+        self.S2stimcmd = S2stimcmd
 
     def increment_file_name(self, filename):
         m = re.search('(d+).h5', filename)
@@ -261,9 +261,7 @@ class Bender:
 
         return self.forcetorque
 
-    def make_motor_stepper_pulses(self, outputfreq = 1000,
-                                scale=6.0,
-                                stepsperrev=6400.0):
+    def make_motor_stepper_pulses(self, outputfreq = 1000, scale=6.0, stepsperrev=6400):
 
         self.outputfreq = outputfreq
         self.scale = scale
@@ -306,14 +304,14 @@ class Bender:
         self.tout = tout
         self.dig = dig
 
-        if self.S1actcmd is None:
-            self.actcmdhi = np.zeros((2, len(tout)))
+        if self.S1stimcmd is None:
+            self.stimcmdhi = np.zeros((2, len(tout)))
         else:
-            S1actcmdhi = interpolate.interp1d(self.t, self.S1actcmd, kind='linear', assume_sorted=True, bounds_error=False,
+            S1stimcmdhi = interpolate.interp1d(self.t, self.S1stimcmd, kind='linear', assume_sorted=True, bounds_error=False,
                                         fill_value=0.0)(tout)
-            S2actcmdhi = interpolate.interp1d(self.t, self.S2actcmd, kind='linear', assume_sorted=True, bounds_error=False,
+            S2stimcmdhi = interpolate.interp1d(self.t, self.S2stimcmd, kind='linear', assume_sorted=True, bounds_error=False,
                                         fill_value=0.0)(tout)
-            self.actcmdhi = np.vstack((S1actcmdhi, S2actcmdhi))
+            self.stimcmdhi = np.vstack((S1stimcmdhi, S2stimcmdhi))
 
         return tout, dig, motorstep, motordirection
 
@@ -321,9 +319,9 @@ class Bender:
         self.inchannels = inchannels
         self.inchannel_names = inchannel_names
 
-    def set_activation_channels(self, S1activation_chan, S2activation_chan):
-        self.S1activation_chan = S1activation_chan
-        self.S2activation_chan = S2activation_chan
+    def set_stimulation_channels(self, S1stimulation_chan, S2stimulation_chan):
+        self.S1stimulation_chan = S1stimulation_chan
+        self.S2stimulation_chan = S2stimulation_chan
 
     def set_motor_channel(self, motor_control_chan):
         self.motor_control_chan = motor_control_chan
@@ -384,8 +382,8 @@ class Bender:
     
     def run(self, device_name):
         inchannels = ['/'.join((device_name, c1)) for c1 in self.inchannels]
-        S1activation_chan = '/'.join((device_name, self.S1activation_chan))
-        S2activation_chan = '/'.join((device_name, self.S2activation_chan))
+        S1stimulation_chan = '/'.join((device_name, self.S1stimulation_chan))
+        S2stimulation_chan = '/'.join((device_name, self.S2stimulation_chan))
         motor_control_chan = '/'.join((device_name, self.motor_control_chan))
         encoder_chan = '/'.join((device_name, self.encoder_chan))
 
@@ -410,8 +408,8 @@ class Bender:
                                                 samps_per_chan=len(self.t))
 
             # set up the analog output channels
-            analog_out.ao_channels.add_ao_voltage_chan(S1activation_chan, 'S1act')
-            analog_out.ao_channels.add_ao_voltage_chan(S2activation_chan, 'S2act')
+            analog_out.ao_channels.add_ao_voltage_chan(S1stimulation_chan, 'S1stim')
+            analog_out.ao_channels.add_ao_voltage_chan(S2stimulation_chan, 'S2stim')
             # it will run much faster than the input channels, because the digital output is linked
             # to it, and it needs to run fast so that the pulses 
             # are output fast enough for smooth motion
@@ -443,7 +441,7 @@ class Bender:
             # write the output
             analog_writer = AnalogMultiChannelWriter(analog_out.out_stream, 
                                                     auto_start=False)
-            analog_writer.write_many_sample(self.actcmdhi)
+            analog_writer.write_many_sample(self.stimcmdhi)
             
             digital_writer = DigitalSingleChannelWriter(digital_out.out_stream,
                                                         auto_start=False)
@@ -603,7 +601,7 @@ class Bender:
         tendramp2 = tstartramp2 + rampdur
 
         if tstartramp1 > 0:
-            pass # actual movement is slower than the ramp, so we won't bother adding the ramp
+            pass # stimual movement is slower than the ramp, so we won't bother adding the ramp
         else:
             # Use boolean assignment
             mask1 = (t >= tstartramp1) & (t < tendramp1)
@@ -624,27 +622,27 @@ class Bender:
         return angle, anglevel, tnorm, freq
 
     # Lots of unused arguments, need to check on that later.        
-    def _make_stimuli(self, is_activation, phase_by_cycle, activation_pulse_rate, prestim_time, poststim_time, prepoststim_dur, prepoststim_sep, actburstdur, duty_by_cycle, freq_by_cycle, movedur):
-        # ... (imports, accessing self.t, self.tnorm, initialization of arrays S1actcmd, etc.) ...
+    def _make_stimuli(self, is_stimulation, phase_by_cycle, stimulation_pulse_rate, prestim_time, poststim_time, prepoststim_dur, prepoststim_sep, stimburstdur, duty_by_cycle, freq_by_cycle, movedur):
+        # ... (imports, accessing self.t, self.tnorm, initialization of arrays S1stimcmd, etc.) ...
         t = self.t 
         tnorm = self.tnorm
-        S1actcmd = np.zeros_like(t)
-        S2actcmd = np.zeros_like(t)
+        S1stimcmd = np.zeros_like(t)
+        S2stimcmd = np.zeros_like(t)
         Lonoff = []
         Ronoff = []
         bendphase = np.zeros_like(tnorm)
-        prepostburst = (np.mod(t * activation_pulse_rate, 1) <= 0.5).astype(float)
+        prepostburst = (np.mod(t * stimulation_pulse_rate, 1) <= 0.5).astype(float)
         prepostburst *= 5.0
 
-        if is_activation:
+        if is_stimulation:
             pulsedur = 0.01         
-            burst = (np.mod(t * activation_pulse_rate, 1) <= 0.5).astype(float)
+            burst = (np.mod(t * stimulation_pulse_rate, 1) <= 0.5).astype(float)
             burst *= 5.0
-            prepostburst = (np.mod(t * activation_pulse_rate, 1) <= 0.5).astype(float)
+            prepostburst = (np.mod(t * stimulation_pulse_rate, 1) <= 0.5).astype(float)
             prepostburst *= 5.0
             bendphase = tnorm - 0.25
 
-        for c, (dur1, duty1, f1, p1) in enumerate(zip(actburstdur, duty_by_cycle, freq_by_cycle, phase_by_cycle)):
+        for c, (dur1, duty1, f1, p1) in enumerate(zip(stimburstdur, duty_by_cycle, freq_by_cycle, phase_by_cycle)):
             if dur1 == 0:
                 continue
             
@@ -662,13 +660,13 @@ class Bender:
             if np.any(bendphase >= c + 0.5 + p1):
                 Ronoff.append(np.array([tstart, tend]) + 0.5 / f1)
 
-            # FIX 1: Replace np.place with boolean assignment for S1actcmd
+            # FIX 1: Replace np.place with boolean assignment for S1stimcmd
             mask1 = (bendphase >= c + p1) & (bendphase < c + p1 + duty1)
-            S1actcmd[mask1] = burst[mask1] # Assign only where the mask is True
+            S1stimcmd[mask1] = burst[mask1] # Assign only where the mask is True
 
-            # FIX 2: Replace np.place with boolean assignment for S2actcmd
+            # FIX 2: Replace np.place with boolean assignment for S2stimcmd
             mask2 = (bendphase >= c + 0.5 + p1) & (bendphase < c + 0.5 + p1 + duty1)
-            S2actcmd[mask2] = burst[mask2] # Assign only where the mask is True
+            S2stimcmd[mask2] = burst[mask2] # Assign only where the mask is True
 
 
         # --- Pre/Post stimulation logic ---
@@ -678,10 +676,10 @@ class Bender:
         
         # FIX 3: Replace np.place with boolean assignment for pre/post stim
         mask3 = (t >= tstart) & (t < tend)
-        S1actcmd[mask3] = prepostburst[mask3] # Assign only where the mask is True
+        S1stimcmd[mask3] = prepostburst[mask3] # Assign only where the mask is True
 
         # Store these in the instance if you need to access them later for analysis
         self.Lonoff = np.array(Lonoff)
         self.Ronoff = np.array(Ronoff)
 
-        return S1actcmd, S2actcmd
+        return S1stimcmd, S2stimcmd
